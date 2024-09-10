15094d0c0476cea16830130d80569c102a83db0c
Kevin Perillo <mootseeker98@gmail.com>
2024-09-10 08:52:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Lies den Inhalt der README-Datei
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='batteryCalculator',  # Name deines Projekts
     version='1.0.3',  # Projektversion
     description='A battery life calculator tool',
-    author='MootSeeker',  # Dein Name oder Teamname
-    author_email='mootseeker98@gmail.com',  # Deine E-Mail-Adresse
+    long_description=long_description,  # Verwende den Inhalt der README-Datei
+    long_description_content_type='text/markdown',  # Gibt an, dass Markdown verwendet wird
+    author='Dein Name',  # Dein Name oder Teamname
+    author_email='deine.email@example.com',  # Deine E-Mail-Adresse
     packages=find_packages(),  # Automatisches Finden von Unterpaketen
-    install_requires=[],  # Hier kannst du Abhängigkeiten angeben, z.B. ['tkinter', 'numpy']
+    install_requires=[],  # Hier kannst du Abhängigkeiten angeben
     entry_points={
         'console_scripts': [
             'batteryCalculator = calculator:main',  # Falls du ein CLI-Tool hast
